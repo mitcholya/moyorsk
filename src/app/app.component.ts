b61f0bb5b56633cebef1506b6f83b1b7474db628
MitchOlya <mitch-gay@yandex.ru>
2017-04-09 19:47:20 +0500

    to working with main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor() {
+
+  } 
+
+  toGooglePaly() {
+    window.location.href='https://play.google.com/store/apps/details?id=ru.moygorod.orsk';
+  }
+
+  toAppleStore() {
+    window.location.href='https://itunes.apple.com/us/app/id1207930011?l=ru&ls=1&mt=8';
+  }
 }
